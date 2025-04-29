9d60c58632829bb39d2141d87ee052959dc025c9
L-23-git <18534119996@163.com>
2025-04-29 23:40:33 +0800

    Update CartService.java

--- E-CommercePlatform/src/main/java/com/online/ecommercePlatform/service/CartService.java
+++ E-CommercePlatform/src/main/java/com/online/ecommercePlatform/service/CartService.java
@@ -1,7 +1,10 @@
 package com.online.ecommercePlatform.service;
 
+import com.online.ecommercePlatform.dto.CartAddDTO;
+import com.online.ecommercePlatform.dto.CartAddResponseDTO;
 import com.online.ecommercePlatform.pojo.PageBean;
 import com.online.ecommercePlatform.pojo.Product;
+import com.online.ecommercePlatform.pojo.Result;
 
 import java.util.List;
 
@@ -9,4 +12,12 @@ public interface CartService {
 
     //查询用户购物车列表
     List<Product> cartList();
+    
+    /**
+     * 将商品添加到购物车
+     * @param userId 用户ID
+     * @param cartAddDTO 添加购物车请求DTO
+     * @return 添加购物车响应结果
+     */
+    Result<CartAddResponseDTO> addToCart(Long userId, CartAddDTO cartAddDTO);
 }
