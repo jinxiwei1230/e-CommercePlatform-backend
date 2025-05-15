b622c88937d29ded9f7030492db82ec1f2534001
L-23-git <18534119996@163.com>
2025-05-15 15:26:52 +0800

    Add files via upload

---
+++ E-CommercePlatform/src/main/java/com/online/ecommercePlatform/vo/FavoriteVo.java
@@ -0,0 +1,11 @@
+package com.online.ecommercePlatform.vo;
+
+import lombok.Data;
+
+
+@Data
+public class FavoriteVo {
+    private String name;
+    private String categoryId;
+    private String num;
+}
